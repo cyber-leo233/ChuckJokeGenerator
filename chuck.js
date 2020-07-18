dad4ae0a92f74d9d7053f0b414b29c18473e911d
Nate Dierkes <googeg89@gmail.com>
2020-07-18 16:38:24 -0400

    initial commit

---
+++ chuck.js
@@ -0,0 +1,28 @@
+
+let getRandomJoke = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','http://api.icndb.com/jokes/random',true);
+
+    xhr.onload = function () {
+      if(this.status === 200){
+          const joke = JSON.parse(this.responseText);
+          displayJoke(joke.value.joke);
+          
+          
+      }
+
+    }
+    xhr.send();
+}
+
+
+
+document.getElementById('get-joke').addEventListener('click',getRandomJoke);
+
+
+
+function displayJoke(joke){
+      document.getElementById('output').innerHTML = `<strong>${joke}</strong>`
+}
+
+
